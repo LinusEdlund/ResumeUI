9f5ec92861043fa7330fbc3c3f00d7e814eebe59
Linus Edlund <Linus.Edlund@hotmail.com>
2024-01-15 12:56:03 +0100

    lia bild

--- scripts.js
+++ scripts.js
@@ -49,7 +49,7 @@ const nav = document.querySelector('.top');
 
 
 
-const projectsData = [{name: 'LiaProject', description: 'Användaren kan skapa sin egna AI-bliderbok ', image: '', repo: 'https://github.com/MarcRick/LiaProject', live: "https://github.com/MarcRick/LiaProject"}, 
+const projectsData = [{name: 'LiaProject', description: 'Användaren kan skapa sin egna AI-bliderbok ', image: 'Lia.png', repo: 'https://github.com/MarcRick/LiaProject', live: "https://github.com/MarcRick/LiaProject"}, 
 {name: 'Turnering', description: 'Skapa din enga cup turnering', image: 'Tunering.png', repo: 'https://github.com/LinusEdlund/Turnering', live: "https://github.com/LinusEdlund/Turnering"},
 {name: 'SuggestionApp', description: 'Användaren kan lägga upp förslag på video ideer. Om man är inloggad som admin får man välja vilka förslag som får komma upp på hemsidan. Det är en kopia av Tim Corys Suggestions hemsida.', image: 'Suggeston.png', repo: 'https://github.com/LinusEdlund/SuggestionApp', live: "https://suggestions.iamtimcorey.com/"}];
 
